0045c712094bd642005a2e454c7380715d06353f
Michel Grootjans <michel.grootjans@gmail.com>
2013-03-25 20:50:55 +0100

    generated rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WarOfWorldcraft::Application.config.secret_token = '274bc2f47c22877b05f92e6294eb6eaacec98d3b382c52f36dfe0d534613bf63976933f68fd0ecb4cd4825c451622b05933ad747b9d7b29b87f4b00b36d9653e'
