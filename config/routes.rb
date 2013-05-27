b2df9ba67289c90329bfa9ca556f8626faae9333
Michel Grootjans <michel.grootjans@gmail.com>
2013-05-27 16:24:53 +0200

    added a few links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 RailsForN00bs::Application.routes.draw do
   root :to => 'zombies#index'
 
-  get "/zombies" => "zombies#index"
-  get "/zombies/:id" => "zombies#show"
+  resources :zombies, only: [:index, :show]
 end
